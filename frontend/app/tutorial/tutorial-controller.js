cffb0655f9d71ba89eef554d48c9dd7e024dcb78
Jeff <jeff.revesz@buzzfeed.com>
2015-08-21 23:41:16 -0400

    fixed git ignore

--- frontend/app/tutorial/tutorial-controller.js
+++ frontend/app/tutorial/tutorial-controller.js
@@ -1,14 +1,27 @@
 require("pixi.js");
 
 function TutorialController() {
-  var stage = new PIXI.Stage(0x66FF99);
-  var renderer = PIXI.autoDetectRenderer(400, 300);
+  var stage = new PIXI.Container(0x66FF99);
+  var renderer = PIXI.autoDetectRenderer(4000, 3000);
   document.body.appendChild(renderer.view);
-  requestAnimFrame(animate);
-  console.log('hello Jen');
+  requestAnimationFrame(animate);
+
+  var texture = PIXI.Texture.fromImage("/static/images/bunny.jpg");
+  var bunny = new PIXI.Sprite(texture);
+
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  bunny.position.x = 200;
+  bunny.position.y = 150;
+
+  stage.addChild(bunny);
 
   function animate() {
-    requestAnimFrame(animate);
+
+    bunny.rotation += 0.1;
+
+    requestAnimationFrame(animate);
     renderer.render(stage);
   }
 }
